6ae2ae2dc683ddc58b9223ec3c52dff5221c9d4f
CUPCAKE <red.delete2@gmail.com>
2016-02-03 11:03:19 +0700

    Create myopenhelper fin

--- app/src/main/java/kru/poompunk/wannachat/weightcheck/MyOpenHelper.java
+++ app/src/main/java/kru/poompunk/wannachat/weightcheck/MyOpenHelper.java
@@ -1,10 +1,35 @@
 package kru.poompunk.wannachat.weightcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by icakeeeeeee on 3/2/2559.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper{
 
-    public MyOpenHelper() {
+    // Explicit
+    public static final String database_name = "Weight.db";//ประกาศค่าคงที่
+    private static final int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key," +
+            "Date text, " +
+            "Weight text);";
+
+
+    public MyOpenHelper(Context context) {
+        super(context,database_name,null,database_version);
     } //constructor
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }// main class
